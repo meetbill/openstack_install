dc9518fa068490e2892389a7e5af067886ce0ffd
meetbill <meetbill@163.com>
2017-07-31 12:14:07 +0800

    update:create_net.sh

--- tools/create_net.sh
+++ tools/create_net.sh
@@ -1,29 +1,49 @@
 #!/bin/bash
-. /etc/openstack-control-script-config/admin-openrc
-neutron net-create --shared --provider:physical_network provider --provider:network_type flat provider
+if [[ ! -f /etc/openstack-control-script-config/main-config.rc  ]] 
+then
+    echo "not config file"
+    exit 1
+fi
+
+. /etc/openstack-control-script-config/main-config.rc
+
+#########################################################################################
+
 START_IP_ADDRESS=192.168.1.31
 END_IP_ADDRESS=192.168.1.40
 DNS_RESOLVER=8.8.8.8
 PROVIDER_NETWORK_GATEWAY=192.168.1.1
 PROVIDER_NETWORK_CIDR=192.168.1.0/24
-neutron subnet-create --name provider --allocation-pool start=$START_IP_ADDRESS,end=$END_IP_ADDRESS --dns-nameserver $DNS_RESOLVER --gateway $PROVIDER_NETWORK_GATEWAY provider $PROVIDER_NETWORK_CIDR
+
+#########################################################################################
+
+if [[ "w$NETWORK_OPT" == "wself-service"  ]]
+then
+    . /etc/openstack-control-script-config/admin-openrc
+    neutron net-create --shared --provider:physical_network provider --provider:network_type flat provider
+    neutron subnet-create --name provider --allocation-pool start=$START_IP_ADDRESS,end=$END_IP_ADDRESS --dns-nameserver $DNS_RESOLVER --gateway $PROVIDER_NETWORK_GATEWAY provider $PROVIDER_NETWORK_CIDR
 
 
 
-. /etc/openstack-control-script-config/demo-openrc
-neutron net-create selfservice
-DNS_RESOLVER=8.8.8.8
-SELFSERVICE_NETWORK_GATEWAY=192.168.5.1
-SELFSERVICE_NETWORK_CIDR=192.168.5.0/24
-neutron subnet-create --name selfservice --dns-nameserver $DNS_RESOLVER --gateway $SELFSERVICE_NETWORK_GATEWAY selfservice $SELFSERVICE_NETWORK_CIDR
+    . /etc/openstack-control-script-config/demo-openrc
+    neutron net-create selfservice
+    DNS_RESOLVER=8.8.8.8
+    SELFSERVICE_NETWORK_GATEWAY=192.168.5.1
+    SELFSERVICE_NETWORK_CIDR=192.168.5.0/24
+    neutron subnet-create --name selfservice --dns-nameserver $DNS_RESOLVER --gateway $SELFSERVICE_NETWORK_GATEWAY selfservice $SELFSERVICE_NETWORK_CIDR
 
 
-. /etc/openstack-control-script-config/admin-openrc
-neutron net-update provider --router:external
-. /etc/openstack-control-script-config/demo-openrc
-neutron router-create router
-neutron router-interface-add router selfservice
-neutron router-gateway-set router provider
+    . /etc/openstack-control-script-config/admin-openrc
+    neutron net-update provider --router:external
+    . /etc/openstack-control-script-config/demo-openrc
+    neutron router-create router
+    neutron router-interface-add router selfservice
+    neutron router-gateway-set router provider
+else
+    . /etc/openstack-control-script-config/admin-openrc
+    neutron net-create --shared --provider:physical_network provider --provider:network_type flat provider
+    neutron subnet-create --name provider --allocation-pool start=$START_IP_ADDRESS,end=$END_IP_ADDRESS --dns-nameserver $DNS_RESOLVER --gateway $PROVIDER_NETWORK_GATEWAY provider $PROVIDER_NETWORK_CIDR
+fi
 
 . /etc/openstack-control-script-config/demo-openrc
 ssh-keygen -q -N ""
